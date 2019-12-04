d64a05cbf7e27c2bd0f2c958649cfb98779d713c
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-04 13:56:54 +0700

    Minor fix

--- install_vim.sh
+++ install_vim.sh
@@ -8,6 +8,6 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 # Install plug-vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-wget -P https://github.com/lamhoangtung/dotfile/raw/master/my_configs.vim ~/.vim_runtime/
+wget https://github.com/lamhoangtung/dotfile/raw/master/my_configs.vim -O ~/.vim_runtime/my_configs.vim
 
-echo "Done. Run vi and run command `:PlugInstall` to finish everything"
+echo "Done. Run vi and run command :PlugInstall to finish everything"
